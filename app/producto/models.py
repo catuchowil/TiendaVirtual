83728c0b93c1bab0fec3c5276f923af3b1442aae
catuchowil <gapuentes@hotmail.com.ar>
2021-12-08 18:49:49 -0300

    Creación de modelos en app productos y migraciones

--- app/producto/models.py
+++ app/producto/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+
+    nombre = models.CharField(max_length=150, verbose_name='Nombre de la Categoría',unique=True,
+                blank=False, null=False)
+    
+
+    def __str__(self):
+        return '{}'.format(self.nombre)
+    
+    
+    class Meta:
+        verbose_name_plural = "Categorias"
+        ordering = ['nombre']
+
+
+class Nacionalidad(models.Model):
+
+    nacionalidad = models.CharField(max_length=150, verbose_name='Nacionalidad Producto',unique=True,
+                blank=False, null=False)
+    
+
+    def __str__(self):
+        return '{}'.format(self.nacionalidad)
+
+    
+    class Meta:
+        verbose_name_plural = "Nacionalidad"
+        ordering = ['nacionalidad']
+
+
+
+class Producto(models.Model):
+    codigo_producto = models.CharField(max_length=10, verbose_name='Código Producto',unique=True)
+    nombre_producto = models.CharField(max_length=150, verbose_name='Nombre Producto', unique=True,)
+    descripcion_producto = models.TextField(blank=True, null=True, verbose_name='Características') 
+    precio_unitario = models.DecimalField(default=0.00, max_digits=9, decimal_places=2,
+                    verbose_name='Precio Unitario')
+    id_categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE,verbose_name='Categoria Producto')
+    id_nacionalidad = models.ForeignKey(Nacionalidad, on_delete=models.CASCADE,verbose_name='Nacionalidad Producto')
+   
+    
+
+    def __str__(self):
+        return '{}{}{}'.format(self.codigo_producto,self.nombre_producto,self.id_categoria)
+    
+
+    class Meta:
+        verbose_name_plural = 'Productos'
+        ordering = ['nombre_producto']
