de28224e1744edd7cfd32366df78233daadbd8a3
catuchowil <gapuentes@hotmail.com.ar>
2021-12-09 12:14:08 -0300

    Creación modelo vendedor - uso de choices

--- app/vendedor/models.py
+++ app/vendedor/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+SUCURSAL =(
+    ('0', 'Seleccionar Sucursal'),
+    ('1', 'Casa Central'),
+    ('2', 'Sucursal Centro 1'),
+    ('3', 'Sucursal Centro 2'),
+    ('4', 'Sucursal Oeste 1'),
+    ('5', 'Sucursal Sur 1'),
+
+
+    )
+
+
+class Vendedor(models.Model):
+    nombre_vendedor = models.CharField(max_length=50, verbose_name='Nombre')
+    apellido_vendedor = models.CharField(max_length=50, verbose_name='Apellido')
+    dni_vendedor = models.CharField(max_length=10, verbose_name='DNI') 
+    domicilio_vendedor = models.CharField(max_length=150, verbose_name='Domicilio')
+    codigo_vendedor = models.CharField(max_length=10, verbose_name='Código',null=True, blank=True)
+    sucursal = models.CharField(max_length=1,verbose_name='Sucursal',choices=SUCURSAL, default='0')
+    
+   
+    
+
+    def __str__(self):
+        return 'Apellido y Nombre: {}, {}'.format(self.apellido_vendedor,self.nombre_vendedor)
+    
+   
+    class Meta:
+        verbose_name_plural = 'Vendedores'
+        ordering = ['apellido_vendedor'] 
+
